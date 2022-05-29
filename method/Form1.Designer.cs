d46138a456c8fcaca953c1572a4a2394ac857140
Eng Ali <Eng Ali@DESKTOP-5J8SEAJ>
2022-05-28 22:53:37 -0700

    Add project files.

---
+++ method/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace method
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtn2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtn1 = new System.Windows.Forms.TextBox();
+            this.txtn3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(348, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtn2
+            // 
+            this.txtn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtn2.Location = new System.Drawing.Point(44, 125);
+            this.txtn2.Multiline = true;
+            this.txtn2.Name = "txtn2";
+            this.txtn2.Size = new System.Drawing.Size(238, 53);
+            this.txtn2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(348, 108);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(238, 292);
+            this.listBox1.TabIndex = 2;
+            
+            // 
+            // txtn1
+            // 
+            this.txtn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtn1.Location = new System.Drawing.Point(44, 49);
+            this.txtn1.Multiline = true;
+            this.txtn1.Name = "txtn1";
+            this.txtn1.Size = new System.Drawing.Size(238, 53);
+            this.txtn1.TabIndex = 3;
+            // 
+            // txtn3
+            // 
+            this.txtn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtn3.Location = new System.Drawing.Point(44, 207);
+            this.txtn3.Multiline = true;
+            this.txtn3.Name = "txtn3";
+            this.txtn3.Size = new System.Drawing.Size(238, 53);
+            this.txtn3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 450);
+            this.Controls.Add(this.txtn3);
+            this.Controls.Add(this.txtn1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtn2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtn2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtn1;
+        private System.Windows.Forms.TextBox txtn3;
+    }
+}
+
---
+++ method/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace method
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //using void with parameters
+        //public void myMethod(String names)
+        //{
+        //    listBox1.Items.Add("My Name Is :" + " " + names);
+
+        //}
+
+
+        //using return type with parameters
+        //public int number(int n1, int n2, int n3)
+        //{
+        //    int sum = n1 + n2+n3;
+        //    return sum;
+        
+
+        //}
+
+
+        //using square
+        //public int square(int num)
+        //{
+        //    int sq = num * num;
+        //    return sq;
+        //}
+
+
+        //using maxValue with parameters
+       public int maxValue(int n1, int n2, int n3)
+        {
+            if(n1> n2 && n1 > n3)
+            {
+                return n1;
+            }else if(n2> n1 && n2 > n3)
+            {
+                return n2;
+            }
+            else if (n3 > n1 && n3 > n2)
+            {
+                return n3;
+            }
+            return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //using void with parameters
+            //myMethod("Ali");
+            //myMethod("Mohamed");
+            //myMethod("Asha");
+
+            //using return type with parameters
+            //int n1 =int.Parse( txtn1.Text);
+            //int n2 = int.Parse(txtn2.Text);
+            //int n3 = int.Parse(txtn3.Text);
+            //listBox1.Items.Add(number(n1, n2, n3));
+
+
+            //using square
+            //int n1 = int.Parse(txtn1.Text);
+            //listBox1.Items.Add(square(n1));
+
+
+            //Using maxValue
+            int n1 = int.Parse(txtn1.Text);
+            int n2 = int.Parse(txtn2.Text);
+            int n3 = int.Parse(txtn3.Text);
+            listBox1.Items.Add(maxValue(n1, n2, n3));
+            txtn1.Text = "";
+            txtn2.Text = "";
+            txtn3.Text = "";
+        }
+
+    }
+}
